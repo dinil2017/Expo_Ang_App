c94412e9b95e76fa5e72a71fd180843f760faa18
Dinil Vidyadharan <dinil.vidyadharan@emirates.com>
2018-10-23 15:15:22 +0400

    sign up module added

---
+++ src/app/auth/signup/signup.component.ts
@@ -0,0 +1,20 @@
+import { NgForm } from '@angular/forms';
+import { AuthService } from './../auth.service';
+import { Component, OnInit, ViewChild } from '@angular/core';
+
+@Component({
+  selector: 'app-signup',
+  templateUrl: './signup.component.html',
+  styleUrls: ['./signup.component.css']
+})
+export class SignupComponent implements OnInit {
+  constructor(private authService: AuthService) {}
+
+  ngOnInit() {}
+
+  signUp(form: NgForm) {
+    const email = form.value.email;
+    const password = form.value.password;
+    this.authService.signupUser(email, password);
+  }
+}
